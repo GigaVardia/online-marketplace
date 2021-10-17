f79675e4319ac5df9c35ad95d596d49234e99b6f
GigaVardia <gigavardia@gmail.com>
2021-10-17 16:39:14 +0400

    added appbar

---
+++ src/components/Home/HeaderComponents/AppBarComponent.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app-bar
+    app
+    flat
+  >
+    <v-container class="pa-0">
+      <v-row no-gutters class="d-flex align-center">
+        <v-col>
+          <v-btn
+            to="/"
+            plain
+            depressed
+            icon
+          >
+            <v-icon>
+              mdi-shopping
+            </v-icon>
+          </v-btn>
+          <router-link to="/">
+            Online shopping
+          </router-link>
+        </v-col>
+        <v-col>
+          <v-autocomplete
+            :menu-props="{ transition: 'slide-y-transition' }"
+            placeholder="Search on ..."
+            hide-no-data
+            return-object
+            hide-details
+            outlined
+            dense
+          >
+          </v-autocomplete>
+        </v-col>
+        <v-col class="d-flex justify-end">
+          <regular-tooltip-button
+            btn-icon="mdi-account"
+            tooltip="Authorization"
+            open-delay="400"
+            bottom
+          >
+          </regular-tooltip-button>
+          <regular-tooltip-button
+            btn-icon="mdi-cart"
+            tooltip="Cart"
+            open-delay="400"
+            bottom
+          >
+          </regular-tooltip-button>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+import RegularTooltipButton from '@/components/Common/RegularTooltipButton'
+
+export default {
+  name: 'AppBarComponent',
+  components: { RegularTooltipButton }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
