7f2cf9eb201471775586c982c92f4c0eb0e4892d
GigaVardia <gigavardia@gmail.com>
2021-10-17 14:31:35 +0400

    removed template files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <hello-world />
+  <v-app>
+    <v-main>
+
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
-
 export default {
-  name: 'Home',
-
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
